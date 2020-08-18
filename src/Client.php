0e5444a3837878304d9477eddb258e35a2ee99df
Tarek Adam <tarek.adam@gmail.com>
2020-08-18 15:03:32 -0600

    off false off false

--- src/Client.php
+++ src/Client.php
@@ -42,14 +42,14 @@ class Client implements Arrayable, Jsonable
 	{
 		$this->api_host = $api;
 
-		$this->guzzle = new Guzzle(['base_uri' => $api, 'auth' => [$token, $secret]]);
+		$this->guzzle = new Guzzle(['base_uri' => $api, 'auth' => [$token, $secret], 'headers' => ['X-on-behalf' => $staff,]]);
 
-		$this->guzzle->setDefaultOption('headers', [
-			'User-Agent'      => 'BoneCreative 0.3 FNKe',
-			'Authorization'   => 'Basic ' . base64_encode($token . ':' . $secret),
-			'X-Forwarded-For' => $client_ip,
-			'X-On-Behalf'     => $staff,
-		]);
+//		$this->guzzle->setDefaultOption('headers', [
+//			'User-Agent'      => 'BoneCreative 0.3 FNKe',
+//			//'Authorization'   => 'Basic ' . base64_encode($token . ':' . $secret),
+//			'X-forwarded-for' => $client_ip,
+//			'X-on-behalf'     => $staff,
+//		]);
 	}
 
 	/**
