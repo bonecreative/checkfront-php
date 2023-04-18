1aa5bf231ba3045d5422a0b22658f834762c63b2
Tarek Adam <tarek.adam@gmail.com>
2023-04-18 15:59:15 -0600

    migrates routes to package

---
+++ routes/grids.php
@@ -0,0 +1,12 @@
+<?php
+
+Route::group(['as' => 'orm::', 'prefix' => 'orm', 'namespace' => '\\App\\Http\\Controllers\\Orm', 'middleware' => ['web', 'auth']], function (){
+
+	FuquIo\LaravelPrototypeUi\Helpers::ResourceGridRouting('check-front-accounts', ['index', 'edit', 'update', 'store'], ['index', 'update', 'destroy']);
+	Route::group(['prefix' => 'check-front-accounts/{check_front_account}'], function (){
+		FuquIo\LaravelPrototypeUi\Helpers::ResourceGridRouting('check-front-sites', ['index', 'edit', 'update', 'store'], ['index', 'update', 'destroy'],null,['','grid']);
+	});
+
+	//@cursor>>
+
+});
