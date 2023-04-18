9a6b3ad506451db68befd97f06756c42f1d37747
Tarek Adam <tarek.adam@gmail.com>
2023-04-18 15:11:40 -0600

    moves copuon caching to package

---
+++ src/Commands/CacheCoupons.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BoneCreative\CheckFront\Commands;
+
+use App\Jobs\CacheCouponsJob;
+use App\Orm\CheckFrontAccount;
+use Illuminate\Console\Command;
+
+class CacheCoupons extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'checkfront:coupons';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		//
+		$accounts = CheckFrontAccount::withoutUserScope()
+		                             ->get();
+
+		foreach($accounts as $account)
+		{
+			CacheCouponsJob::dispatch($account);
+		}
+	}
+}
